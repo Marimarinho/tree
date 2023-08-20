30e5467997d1d3bd5e50cde96def235ceddf10d9
Mariana Marinho <marianalnmarinho@gmail.com>
2023-08-20 20:23:50 -0300

    Atividade de PFE

---
+++ rctt/react-d/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const um = ' Testando '
+  const dois = 'Vite+React'
+  
+  return (
+    <>
+    {um+dois}
+      
+    </>
+  )
+}
+
+export default App
